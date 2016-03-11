e53d5925cdec819855e9fcefcba8b2093ff3dcf5
Maksimenko Vladimir <xupypr@xupypr.com>
2016-03-11 23:17:04 +0300

    fix

--- js/gallery.js
+++ js/gallery.js
@@ -58,7 +58,7 @@ $(function() {
 	
 	Gallery				= (function() {
 			// index of the current item
-		var current			= 1, 
+		var current			= 0, 
 			// mode : carousel || fullview
 			mode 			= 'carousel',
 			// control if one image is being loaded
@@ -75,6 +75,7 @@ $(function() {
 					
 					// show first image
 					_showImage( $items.eq( current ) );
+                                        current = 1;
 						
 				});
 				
